fb13782a8dfc4c1d0f9fa76ca099e02adccea5a3
Jose Aceves <jose4233@gmal.com>
2025-11-21 19:17:46 -0600

    filtro fecha venta

--- sicov/src/main/java/com/jose/sicov/controller/VentaController.java
+++ sicov/src/main/java/com/jose/sicov/controller/VentaController.java
@@ -7,41 +7,49 @@ import com.jose.sicov.service.impl.VentaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1//ventas")
 public class VentaController {
 
-    @Autowired private VentaServiceImpl ventaService;
-    @Autowired private VentaRepository ventaRepository;
+    @Autowired
+    private VentaServiceImpl ventaService;
+    @Autowired
+    private VentaRepository ventaRepository;
 
     @PostMapping
     public ResponseEntity<VentaDTO> registrarVenta(@RequestBody VentaDTO ventaDTO) {
         Venta nuevaVenta = ventaService.registrarNuevaVenta(ventaDTO);
         return new ResponseEntity<>(nuevaVenta.getDto(), HttpStatus.CREATED);
     }
-    
+
     @GetMapping
     public ResponseEntity<Page<VentaDTO>> listarVentas(
-        @RequestParam(required = false) String query, // Búsqueda por Cliente/ID
-        Pageable pageable) 
-    {
-        if (query != null && !query.trim().isEmpty()) {
-            return ResponseEntity.ok(ventaRepository.searchVentas(query.trim(), pageable).map(Venta::getDto));
+            @RequestParam(required = false) String query,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            Pageable pageable) {
+        if ((query != null && !query.trim().isEmpty()) || date != null) {
+
+            String safeQuery = (query != null && !query.trim().isEmpty()) ? query.trim() : "";
+
+            return ResponseEntity.ok(
+                    ventaRepository.searchVentas(safeQuery, date, pageable)
+                            .map(Venta::getDto));
         } else {
             return ResponseEntity.ok(ventaRepository.findAll(pageable).map(Venta::getDto));
         }
     }
-    
+
     @GetMapping("/{id}")
     public ResponseEntity<VentaDTO> obtenerVentaPorId(@PathVariable Long id) {
-        Optional<Venta> venta = ventaRepository.findById(id); 
+        Optional<Venta> venta = ventaRepository.findById(id);
         return ResponseEntity.ok(venta.get().getDto());
     }
 }
-
